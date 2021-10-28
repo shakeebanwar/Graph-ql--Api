c66e4ab9a53c104f1fbc009f8ad16ac6c5cd5468
Mohammed Shoaib <shoaibbilal101@gmail.com>
2021-10-28 16:36:42 +0500

    Api

---
+++ myapp/views.py
@@ -0,0 +1,11 @@
+# from django.shortcuts import render
+# from django.shortcuts import render , HttpResponse , redirect
+# from django.views import View
+# # Create your views here.
+
+
+# class Home1(View):
+
+#     def get(self, request):
+        
+#         return HttpResponse("ok")
