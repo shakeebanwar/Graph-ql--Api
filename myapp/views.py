91a8cfbc0a444a74e5aa84313d20e211cd39ec31
muhammadharis1 <mh7365496@gmail.com>
2021-11-23 15:46:45 +0500

    minor bugs fix

--- myapp/views.py
+++ myapp/views.py
@@ -6,25 +6,51 @@ from django_restql.mixins import DynamicFieldsMixin
 from rest_framework.response import Response
 from myapp.models import *
 
+
+def queryParameter(query):
+  count = 0
+  queryPattern = ['id','title', 'author', 'year_published', 'review', 'Super_Admin_Id']
+  for j in queryPattern:
+      
+      for k in query:
+        if j == k:
+          count = count + 1
+        
+
+  if count == len(query): 
+    return True
+
+  else:
+      return False
+
+
 class UserViewSet(APIView):
     def get(self,request):
-        query = request.GET['query'][1:-1].split(',')
-        start = int(request.GET['start'])
-        end = start + 20
-        totalobj = Book.objects.all().count()
-        if start > totalobj-1:
-            return Response({'status':False,'message':'invalid index'})
+       
 
-        else:
-            queryPattern = ['id','title', 'author', 'year_published', 'review', 'Super_Admin_Id']
-            for j in queryPattern:
-                if not j in query:
+        ###Key Validation
+        if 'query' in request.GET and 'start' in request.GET:
+            query = request.GET['query'][1:-1].split(',')
+            start = int(request.GET['start'])
+            end = start + 20
+            totalobj = Book.objects.all().count()
+            if start > totalobj-1:
+                return Response({'status':False,'message':'invalid index'})
+
+            else:
+                queryPattern = ['id','title', 'author', 'year_published', 'review', 'Super_Admin_Id']
+                status = queryParameter(query)
+                if not status:        
                     return Response({'status':False,'message':'invalid query parameter'})
-
-            queryset = Book.objects.values(*query)[start:]
-            print("objlength is ",len(queryset))
-            return Response({'status':True,'data':queryset,'end':end,'total':totalobj})
-
+                
+                else:
+                    queryset = Book.objects.values(*query)[start:end]
+                    return Response({'status':True,'data':queryset,'end':end,'total':totalobj})
+        else:
+            return Response({'status':False,'message':'query or start key is required'})
+    
+    
+    
     
     
     def post(self,request):
